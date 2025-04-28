e53aa892af5c3faa2ae4d81523f59eb7dc7176b0
ArthurDevWorks <232036770@aluno.unb.br>
2025-04-28 13:18:24 -0300

    :sparkles: feat Criada classe pessoa com tipo de dado sexo como Enum

---
+++ src/Pessoa.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Pessoa {
+    private String nome;
+    private Date nascimento;
+    private  Sexo sexo;
+    private String telefone;
+
+    public Pessoa(String nome, Date nascimento, Sexo sexo, String telefone) {
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.sexo = sexo;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
